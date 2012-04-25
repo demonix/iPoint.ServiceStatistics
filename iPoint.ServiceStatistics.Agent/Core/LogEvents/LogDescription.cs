e53c46f39ff49da2762f0c2db55763662e2f25ef
demonix <demonixed@gmail.com>
2012-04-25 12:06:32 +0600

    refactor

--- iPoint.ServiceStatistics.Agent/Core/LogEvents/LogDescription.cs
+++ iPoint.ServiceStatistics.Agent/Core/LogEvents/LogDescription.cs
@@ -26,17 +26,7 @@ namespace iPoint.ServiceStatistics.Agent.Core.LogEvents
             LogDirectory = logDirectory;
         }
 
-        private static List<LogEventDescription> GetLogEventDescrptions(string directoryName)
-        {
-            List<LogEventDescription> result = new List<LogEventDescription>();
-            FileInfo[] fileInfos = new DirectoryInfo(directoryName).GetFiles();
-            foreach (FileInfo fileInfo in fileInfos)
-            {
-                result.Add(LogEventDescription.LoadFromConfigFile(fileInfo.FullName));
-            }
-            return result;
-        }
-
+     
         public static IEnumerable<LogDescription> ProcessLogDescription(string fileName)
         {
             _logger.Info("Processing LogDescription " + fileName);
@@ -44,20 +34,33 @@ namespace iPoint.ServiceStatistics.Agent.Core.LogEvents
 
             string fileMask = settingsReader.GetConfigParam("FileMask");
             string encoding = settingsReader.GetConfigParam("Encoding");
-            List<string> possibleLogDirectories =
-                settingsReader.GetConfigParams("LogDirectory").SelectMany(FilePathHelpers.FindDirectoriesOnFixedDisks).ToList();
-            List<LogEventDescription> logEventDescriptions =
-                GetLogEventDescrptions(Path.ChangeExtension(fileName, "EventDescripions"));
-            _logger.Info("Total " + possibleLogDirectories.Count + " log directories: " +
-                         String.Join("\r\n", possibleLogDirectories.ToArray()));
 
-            foreach (string logDirectory in possibleLogDirectories)
+
+            IEnumerable<string> possibleLogDirectories = settingsReader.GetConfigParams("LogDirectory").SelectMany(FilePathHelpers.FindDirectoriesOnFixedDisks);
+            List<string> directories = possibleLogDirectories.SelectMany(FilePathHelpers.GetDirectoriesByMaskedPath).Distinct().ToList();
+            _logger.Info(String.Format("Total {0} log directories:\r\n{1}", directories.Count, String.Join("\r\n", directories.ToArray())));
+            string eventDescriptionsDir = Path.ChangeExtension(fileName, "EventDescriptions");
+            List<LogEventDescription> logEventDescriptions = GetLogEventEvaluationRules(eventDescriptionsDir);
+
+            foreach (string logDirectory in directories)
             {
                 foreach (string path in FilePathHelpers.GetDirectoriesByMaskedPath(logDirectory))
                 {
                     yield return new LogDescription(fileName, fileMask, encoding, logEventDescriptions, path);
                 }
             }
+        }
+
+        private static List<LogEventDescription> GetLogEventEvaluationRules(string directoryName)
+        {
+            List<LogEventDescription> result = new List<LogEventDescription>();
+            if (!Directory.Exists(directoryName)) return result;
+            FileInfo[] fileInfos = new DirectoryInfo(directoryName).GetFiles();
+            foreach (FileInfo fileInfo in fileInfos)
+            {
+                result.Add(LogEventDescription.LoadFromConfigFile(fileInfo.FullName));
+            }
+            return result;
         }
 
         public string LogDirectory { get; private set; }
